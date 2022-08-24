2f842c73d4cc90ad9a4525df5e2dfed5dae4bcd1
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-08-24 16:53:03 -0300

    feature: criando a funcionalidade de aplaudir dando mais um ao clicar

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,4 +1,5 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import style from './Comment.module.css';
 
@@ -8,8 +9,14 @@ interface CommentProps {
 }
 
 export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -38,9 +45,9 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
