8f0a082214a00027a04d0220ccbd7b49dfdc6bb2
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-08-25 07:23:19 -0300

    refactor: refatorando o componente Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,17 @@
+import { ImgHTMLAttributes } from 'react';
 import style from './Avatar.module.css';
 
 //https://github.com/gabrieljuniorferrari88.png
 
-interface AvatarProps {
-  src: string;
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
 }
 
-export function Avatar({ src, hasBorder }: AvatarProps) {
+export function Avatar({ hasBorder, ...rest }: AvatarProps) {
   return (
     <img
       className={hasBorder ? style.avatarWithBorder : style.avatar}
-      src={src}
+      {...rest}
     />
   );
 }
